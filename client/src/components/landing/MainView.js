13070a9cf808b1884c9e00320508f86fb462a2dd
Manbir Jaspal <manb.jaspal@gmail.com>
2019-12-14 17:35:04 -0500

    uploading latest code

--- client/src/components/landing/MainView.js
+++ client/src/components/landing/MainView.js
@@ -20,22 +20,6 @@ class MainView extends React.Component {
   componentDidMount() {
       console.log(this.props);
       this.props.getGroups();
-
-
-    // console.log(this.state);
-    // if(this.state.viewState === "groups") {
-    //     axios.get(url + "groups").then(
-    //     function(response) {
-    //       this.setState({ groups: response.data });
-    //       }.bind(this),
-    //         function(error) {
-    //           console.log(error)
-    //                 }
-    //             );
-    //           }
-
-
-
             }
 
   renderList() {
@@ -60,68 +44,6 @@ class MainView extends React.Component {
       </div>
     );
   }
-
-
-  // onTermSubmit = (term) => {
-  //
-  // }
-  //
-  // onGetPosts = (response) => {
-  //   console.log(response.data[0].group_id);
-  //   this.setState({posts: response.data, viewState: 'posts'});
-  // }
-  //
-  // onGroupSelect(id, onGetPosts) {
-  //   console.log("in click handler - "+id);
-  //   axios.get(url + "posts", {params: {
-  //       groupId: id  }}).then(
-  //     function(response) {
-  //       onGetPosts(response);
-  //     },
-  //     function(error) {
-  //       console.log(error);
-  //     }
-  //   );
-  // }
-  //
-  // onGetComments = (response) => {
-  //   this.setState({comments: response.data, viewState: 'comments'});
-  // }
-  //
-  // onPostSelect(id, onGetComments) {
-  //   console.log("in click handler - "+id);
-  //   axios.get(url + "comments", {params: {
-  //       postId: id  }}).then(
-  //     function(response) {
-  //       onGetComments(response);
-  //     },
-  //     function(error) {
-  //       console.log(error);
-  //     }
-  //   );
-  // }
-  //
-  //
-  // render() {
-  //   return (
-  //     <div>
-  //       <Header/>
-  //       <div className="ui container">
-  //         <SearchBar onFormSubmit={this.onTermSubmit} />
-  //         {this.state.viewState === 'groups' && this.state.groups.length > 0 ?
-  //           <GroupList groups={this.state.groups} onGroupSelect={this.onGroupSelect} onGetPosts={this.onGetPosts}/>
-  //         : null}
-  //         {this.state.viewState === 'posts' && this.state.posts.length > 0?
-  //           <PostsList posts={this.state.posts} onPostSelect={this.onPostSelect} onGetComments={this.onGetComments}/>
-  //         : null}
-  //         {this.state.viewState === 'comments' && this.state.comments.length > 0?
-  //           <PostDetail comments={this.state.comments}/>
-  //         : null}
-  //
-  //       </div>
-  //     </div>
-  //             );
-  //           }
 }
 
 const mapStateToProps = state => {
